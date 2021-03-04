6000627c708a238a7f52a09fbf76d22f88c99855
PC <PC@CALDERAS>
2021-03-04 13:36:56 -0600

    .

---
+++ Primer-Parcial/App1/App1/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Primer_Parcial
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+            string num = (numero.Text);
+            num = num.Remove(num.Length - 1);
+            int x, y;
+
+            y = int.Parse(num);
+            x = y - 4;
+
+
+            DisplayAlert("El Número que pensaste es:", x.ToString(), "Fin...");
+
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Piensa en un numero y anotalo donde tu quieras";
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Ahora multiplica el numero que pensaste por 2";
+        }
+
+        private void Button_Clicked_3(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Sumale 8 al resultado aneterior";
+        }
+
+        private void Button_Clicked_4(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Al resultado que llevas acumulado multiplicalo por 5";
+        }
+
+        private void Button_Clicked_5(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Ingresa el resultado que acumulaste en los pasos aterirores";
+        }
+    }
+
+}
+
+
---
+++ Primer-Parcial/App2/App2/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+            int resul = int.Parse(numero.Text);
+
+            int a, b;
+            a = resul - 365; 
+            string aStrign = a.ToString();
+            aStrign = aStrign.Remove(aStrign.Length - 2);
+
+            b = resul - 365;
+            string bString = b.ToString();
+            bString = bString.Remove(bString.Length - 4, 2);
+
+            DisplayAlert("El Mes de tu cumpleaños es:", bString.ToString(), "Toca para ver tu mes");
+            DisplayAlert("El dia de tu cumpleaños es:", aStrign.ToString(), "Fin...");
+            
+
+
+
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Anota el día en que naciste donde tu quieras";
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Ahora el día multiplicalo por veinte (20)";
+        }
+
+        private void Button_Clicked_3(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Al resultado sumale setenta y tres (73)";
+        }
+
+        private void Button_Clicked_4(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Ahora multiplicalo por 5";
+        }
+
+        private void Button_Clicked_5(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Posteriormente sumale la fecha del mes en que naciste a tu resultado.";
+        }
+
+        private void Button_Clicked_6(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Ingresa el resultado que llevas acumulado en total";
+        }
+    }
+}
